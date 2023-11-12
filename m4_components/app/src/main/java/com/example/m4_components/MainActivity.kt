7ac2d906a1e397dc17b0235c9e43131cbc9da32c
Марков Алексей <aleks-mark-dzr@yandex.ru>
2023-11-12 19:24:57 +0300

    correct switch code on m4_components layout

--- m4_components/app/src/main/java/com/example/m4_components/MainActivity.kt
+++ m4_components/app/src/main/java/com/example/m4_components/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.m4_components
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.widget.doOnTextChanged
+import com.example.m4_components.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.nameText.doOnTextChanged { text, _, _, count -> }
     }
 }
