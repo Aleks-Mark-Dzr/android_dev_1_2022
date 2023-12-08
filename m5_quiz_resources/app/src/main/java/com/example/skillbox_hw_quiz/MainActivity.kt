51ec8b7fa081729cdeb83e7445be9c2446ab9f8e
Марков Алексей <aleks-mark-dzr@yandex.ru>
2023-12-08 15:36:25 +0300

    add m5_quiz_resources

--- m5_quiz_resources/app/src/main/java/com/example/skillbox_hw_quiz/MainActivity.kt
+++ m5_quiz_resources/app/src/main/java/com/example/skillbox_hw_quiz/MainActivity.kt
@@ -8,11 +8,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
+        setContentView(R.layout.activity_welcome)
+//        if (savedInstanceState == null) {
+//            supportFragmentManager.beginTransaction()
+//                .replace(R.id.container, MainFragment.newInstance())
+//                .commitNow()
+//        }
     }
 }
